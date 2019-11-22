3e4c1a592c6851bab072e830970e37ee64cc1fe0
Broderick Koo <broderick.koo@cloverhealth.com>
2019-11-22 14:31:07 +0800

    added `coordinatesGenerator.isInCircle`

--- src/coordinatesGenerator.test.ts
+++ src/coordinatesGenerator.test.ts
@@ -1,7 +1,7 @@
-import {generateCoordinates} from "./coordinatesGenerator";
+import {generateCoordinates, isInCircle} from "./coordinatesGenerator";
 
 describe("coordinatesGenerator", function () {
-    it("should generate coordinate", () => {
+    it("should generate coordinates", () => {
         // given
         const generator = (function* () {
             yield 0.991802431776998;
@@ -18,5 +18,19 @@ describe("coordinatesGenerator", function () {
             x: 0.991802431776998,
             y: 0.6724734436367914,
         })
+    });
+
+    it("should check if coordinates is inside circle", () => {
+        // given
+        const coordinates = {
+            x: 0.991802431776998,
+            y: 0.6724734436367914,
+        };
+
+        // when
+        const inCircle = isInCircle({coordinates});
+
+        // then
+        expect(inCircle).toEqual(false);
     });
 });
--- src/coordinatesGenerator.ts
+++ src/coordinatesGenerator.ts
@@ -1,6 +1,16 @@
-export function generateCoordinates({random = Math.random}: { random: () => number }) {
+interface MyCoordinates {
+    x: number,
+    y: number
+}
+
+export function generateCoordinates({random = Math.random}: { random: () => number }): MyCoordinates {
     return {
         x: random(),
         y: random(),
     }
+}
+
+export function isInCircle({coordinates: {x, y}}: { coordinates: MyCoordinates }) {
+    const radius = 1;
+    return Math.sqrt(x * x + y * y) < radius;
 }
