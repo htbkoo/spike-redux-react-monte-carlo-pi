1c921ce1cf5a324279fb228ca9d9a4a287e061a5
htbkoo <h1017760@connect.hku.hk>
2019-12-08 00:16:53 +0800

    added demonstration usage of plotly.js

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,21 +1,70 @@
 import React from 'react';
+import range from 'lodash/range';
+import groupBy from 'lodash/groupBy';
 
 import Plot from 'react-plotly.js';
+import {generateCoordinates, isInCircle, MyCoordinates} from "./coordinatesGenerator";
 
 export default function Canvas() {
+    const {inside, outside} = generateData({count: 1000});
+    console.log(inside);
     return (
         <Plot
             data={[
                 {
-                    x: [1, 2, 3],
-                    y: [2, 6, 3],
+                    ...asPlotData(inside),
                     type: 'scatter',
-                    mode: 'lines+markers',
+                    mode: 'markers',
+                    marker: {color: 'blue'},
+                },
+                {
+                    ...asPlotData(outside),
+                    type: 'scatter',
+                    mode: 'markers',
                     marker: {color: 'red'},
                 },
-                {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
             ]}
-            layout={{width: 320, height: 240, title: 'A Fancy Plot'}}
+            layout={{
+                width: 800,
+                height: 800,
+                title: 'Trying to demonstrate estimation of Pi with Monte Carlo algorithm',
+                shapes: [
+                    {
+                        type: 'circle',
+                        xref: 'x',
+                        yref: 'y',
+                        // fillcolor: 'lightblue',
+                        x0: -1,
+                        y0: -1,
+                        x1: 1,
+                        y1: 1,
+                        line: {
+                            color: 'lightblue'
+                        }
+                    },
+                ],
+                "xaxis.range": [0, 1],
+                "yaxis.range": [0, 0.1],
+                "xaxis": {"fixedrange": true, rangemode: "nonnegative"},
+                "yaxis": {"fixedrange": true, rangemode: "nonnegative"},
+                "xaxis.autorange": false,
+                "yaxis.autorange": false,
+            }}
         />
     );
+
+    function generateData({count}: { count: number }): { inside: MyCoordinates[], outside: MyCoordinates[] } {
+        const coordinatesPairs = range(count).map(() => generateCoordinates());
+        const {true: inside, false: outside} = groupBy(coordinatesPairs, coordinates => isInCircle({coordinates}));
+
+        return {inside, outside};
+    }
+
+    function asPlotData(coordinatesPairs: MyCoordinates[]): { x: number[], y: number[] } {
+        return coordinatesPairs.reduce((obj, coordinates) => {
+            obj.x.push(coordinates.x);
+            obj.y.push(coordinates.y);
+            return obj;
+        }, {x: [] as number[], y: [] as number[]})
+    }
 }
