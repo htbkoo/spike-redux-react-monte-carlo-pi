8a7715cd10211d3dd099b5c99608289620ba32ee
htbkoo <h1017760@connect.hku.hk>
2020-02-08 18:02:10 +0800

    grouped test in test suites

--- src/coordinatesGenerator.test.ts
+++ src/coordinatesGenerator.test.ts
@@ -3,46 +3,52 @@ import range from 'lodash/range';
 import {generateCoordinates, isInCircle} from "./coordinatesGenerator";
 
 describe("coordinatesGenerator", function () {
-    it("should generate coordinates", () => {
-        // given
-        const generator = (function* () {
-            yield 0.991802431776998;
-            yield 0.6724734436367914;
-        })();
+    describe("generateCoordinates", () => {
+        it("should generate coordinates", () => {
+            // given
+            const generator = (function* () {
+                yield 0.991802431776998;
+                yield 0.6724734436367914;
+            })();
 
-        const mockRandomFunc = () => generator.next().value;
+            const mockRandomFunc = () => generator.next().value;
 
-        // when
-        const coordinates = generateCoordinates({random: mockRandomFunc});
+            // when
+            const coordinates = generateCoordinates({random: mockRandomFunc});
 
-        // then
-        expect(coordinates).toEqual({
-            x: 0.991802431776998,
-            y: 0.6724734436367914,
-        })
+            // then
+            expect(coordinates).toEqual({
+                x: 0.991802431776998,
+                y: 0.6724734436367914,
+            })
+        });
     });
 
-    it("should check if coordinates is inside circle", () => {
-        // given
-        const coordinates = {
-            x: 0.991802431776998,
-            y: 0.6724734436367914,
-        };
+    describe("isInCircle", () => {
+        it("should check if coordinates is inside circle", () => {
+            // given
+            const coordinates = {
+                x: 0.991802431776998,
+                y: 0.6724734436367914,
+            };
 
-        // when
-        const inCircle = isInCircle({coordinates});
+            // when
+            const inCircle = isInCircle({coordinates});
 
-        // then
-        expect(inCircle).toEqual(false);
+            // then
+            expect(inCircle).toEqual(false);
+        });
     });
 
-    it("should estimate pi", () => {
-        // const COUNT = 10, BATCH = 10;
-        // const COUNT = 1000000, BATCH = 10;
-        const COUNT = 10000000, BATCH = 10;
-        const inCounts = range(COUNT).filter(() => isInCircle({coordinates: generateCoordinates()})).length;
-        const estimatedPI = 4 * inCounts / COUNT;
-        const error = (Math.abs(Math.PI - estimatedPI) / Math.PI) * 100;
-        console.log(`COUNT: ${COUNT}, inCounts: ${inCounts}, estimated PI: ${estimatedPI}, error: ${error}%`);
+    describe("MISC", () => {
+        xit("should estimate pi", () => {
+            // const COUNT = 10, BATCH = 10;
+            // const COUNT = 1000000, BATCH = 10;
+            const COUNT = 10000000;
+            const inCounts = range(COUNT).filter(() => isInCircle({coordinates: generateCoordinates()})).length;
+            const estimatedPI = 4 * inCounts / COUNT;
+            const error = (Math.abs(Math.PI - estimatedPI) / Math.PI) * 100;
+            console.log(`COUNT: ${COUNT}, inCounts: ${inCounts}, estimated PI: ${estimatedPI}, error: ${error}%`);
+        });
     });
 });
